203b9a4d3f5241cd0ce56b54b46554571aaca8b7
matengfei <matengfei@git.com>
2022-07-06 17:09:08 +0800

    --

--- src/main/java/com/test/git/tomato.java
+++ src/main/java/com/test/git/tomato.java
@@ -3,12 +3,7 @@ package com.test.git;
 public class tomato {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("ÄãºÃ£¡£¡£¡");
-		
-=======
-		System.out.println("hello world!!!");
->>>>>>> branch 'master' of https://github.com/xmg1995git/GitTest.git
+
 	}
 
 }
